0b86a3bab83d13c7df9aa003abf85c68ba1aa80d
JRoy <10731363+JRoy@users.noreply.github.com>
2025-08-03 14:39:00 -0700

    format config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,15 +7,13 @@ import { fileURLToPath } from 'node:url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all,
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
 export default defineConfig([
-    {
-        extends: compat.extends(
-            'plugin:prettier/recommended',
-        ),
-    },
+  {
+    extends: compat.extends('plugin:prettier/recommended'),
+  },
 ]);
